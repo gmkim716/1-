a83f83a9d0b628a7794eab9e4b57eb8c88d4dd76
Jayden, Gyeongmin Kim <starlight3775@gmail.com>
2022-11-16 17:48:34 +0900

    gyeongmin update

--- back-server/movies/urls.py
+++ back-server/movies/urls.py
@@ -5,7 +5,7 @@ app_name='movies'
 urlpatterns = [
     # path('', views.index, name='index'),
     # path('popular/', views.get_popular, name='popular'),
-    # path('tmdb/', views_tmdb.tmdb_data),
+    path('tmdb/', views_tmdb.tmdb_data),
     # path('get_youtube/', views.get_youtube),
     path('api/v1/', views.movie_list)
 ]
--- back-server/movies/views_tmdb.py
+++ back-server/movies/views_tmdb.py
@@ -6,12 +6,17 @@ from .models import Genre, Movie, Actor
 - fixtures/movie.json 파일 생성
 $ mkdir movies/fixtures
 $ python -Xutf8 manage.py dumpdata movies --indent 4 > ./movies/fixtures/movies.json
+$ python manage.py loaddata movies.json
 '''
 
 API_KEY = '1894a2923867c8d04cf110591f18e4c0'                # TMDB key
 GENRE_URL = 'https://api.themoviedb.org/3/genre/movie/list'
 POPULAR_MOVIE_URL = 'https://api.themoviedb.org/3/movie/popular'
+NOWPLAYING_MOVIE_URL = 'https://api.themoviedb.org/3/movie/now_playing' 
+UPCOMING_MOVIE_URL = 'https://api.themoviedb.org/3/movie/upcoming'
+LATEST_MOVIE_URL = 'https://api.themoviedb.org/3/movie/latest'
 
+id_list = []
 
 # TMDB 장르 json 데이터 반환
 def tmdb_genres():
@@ -31,9 +36,8 @@ def tmdb_genres():
         )
     return JsonResponse(response)       # json 데이터를 반환
 
-
 # 유투브 키 가져오기
-def get_youtube_key(movie_dict):    
+def get_youtube_key(movie_dict):
     movie_id = movie_dict.get('id')
     response = requests.get(           # 해당 URL로부터 params로 요청을 보내 json 데이터를 얻어오기             
         f'https://api.themoviedb.org/3/movie/{movie_id}/videos',
@@ -46,7 +50,7 @@ def get_youtube_key(movie_dict):
             return video.get('key')                 # key를 반환
     return 'nothing'                  # 해당이 없는 경우 
 
-
+# 배우 목록 가져오기
 def get_actors(movie):
     movie_id = movie.id
     response = requests.get(
@@ -66,11 +70,83 @@ def get_actors(movie):
                 name=person.get('name')
             )
         movie.actors.add(actor_id)          # artors에 추가    
-        if movie.actors.count() == 5:       # 5명의 배우 정보만 저장
+        if movie.actors.count() == 3:       # 3명의 배우 정보만 저장
             break
-
-
+ 
+# 영화 데이터 가져오기 # 11/16    
 def movie_data(page=1):
+    
+    for url in [LATEST_MOVIE_URL, NOWPLAYING_MOVIE_URL, POPULAR_MOVIE_URL, UPCOMING_MOVIE_URL]:
+        print('url:', url)
+        response = requests.get(
+        url,                  # api 종류별 사용
+        params={
+            'api_key': API_KEY,
+            'language': 'ko-kr',     
+            'page': page,       
+        }
+        ).json()
+        if url == LATEST_MOVIE_URL:
+            # 유투브 key 조회
+            youtube_key = get_youtube_key(response)
+                
+
+            if not response.get('release_date'): continue   # 없는 필드 skip
+            if not response.get('poster_path'): continue
+            if not response.get('id'): continue
+            if response.get('id') in id_list: continue
+                            
+            movie = Movie.objects.create(
+                id = response.get('id'),
+                title = response.get('title'),
+                release_date = response.get('release_date'),
+                popularity = response.get('popularity'),
+                vote_count = response.get('vote_count'),
+                vote_average = response.get('vote_average'),
+                overview = response.get('overview'),
+                poster_path = response.get('poster_path'),   
+                youtube_key = youtube_key         
+            )
+
+            for genre_id in response.get('genre_ids', []):
+                movie.genres.add(genre_id)
+
+            # 배우들 저장
+            get_actors(movie)
+            # print('>>>', movie.title, '==>', movie.youtube_key) 
+
+        else:    
+            for movie_dict in response.get('results'):
+                if not movie_dict.get('release_date'): continue   # 없는 필드 skip
+                if not movie_dict.get('poster_path'): continue
+                if not movie_dict.get('id'): continue
+                if movie_dict.get('id') in id_list: continue
+                id_list.append(movie_dict.get('id'))
+                print(id_list)  # 확인
+                
+                # 유투브 key 조회
+                youtube_key = get_youtube_key(movie_dict)
+
+                movie = Movie.objects.create(
+                    id = movie_dict.get('id'),
+                    title = movie_dict.get('title'),
+                    release_date = movie_dict.get('release_date'),
+                    popularity = movie_dict.get('popularity'),
+                    vote_count = movie_dict.get('vote_count'),
+                    vote_average = movie_dict.get('vote_average'),
+                    overview = movie_dict.get('overview'),
+                    poster_path = movie_dict.get('poster_path'),   
+                    youtube_key = youtube_key         
+                )
+                for genre_id in movie_dict.get('genre_ids', []):
+                    movie.genres.add(genre_id)
+
+                # 배우들 저장
+                get_actors(movie)
+                # print('>>>', movie.title, '==>', movie.youtube_key) 
+
+'''
+    # POPULAR_MOVIE_URL로부터 가져온 영화 데이터
     response = requests.get(
         POPULAR_MOVIE_URL,                  # popular api 사용
         params={
@@ -79,9 +155,14 @@ def movie_data(page=1):
             'page': page,       
         }
     ).json()
-
     for movie_dict in response.get('results'):
         if not movie_dict.get('release_date'): continue   # 없는 필드 skip
+        if not movie_dict.get('poster_path'): continue
+        if not movie_dict.get('id'): continue
+        if movie_dict.get('id') in id_list: continue
+        id_list.append(movie_dict.get('id'))
+        print(id_list)  # 확인
+        
         # 유투브 key 조회
         youtube_key = get_youtube_key(movie_dict)
 
@@ -101,15 +182,24 @@ def movie_data(page=1):
 
         # 배우들 저장
         get_actors(movie)
-        print('>>>', movie.title, '==>', movie.youtube_key)    
+        # print('>>>', movie.title, '==>', movie.youtube_key)    
+'''
 
 
+
+
+
+
+# TMDB 데이터 가져오기
 def tmdb_data(requests):
-    Genre.objects.all().delete()
+    # 기존 데이터가 있다면 초기화
+    Genre.objects.all().delete()        
     Actor.objects.all().delete()
     Movie.objects.all().delete()
 
     tmdb_genres()
-    for i in range(1, 6):
+    global id_list          # 전역변수로 선언: 리스트- 영화 id를 담는 곳 
+    for i in range(1, 6):       # 페이지 1~5 페이지 
         movie_data(i)
+
     return HttpResponse('OK >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
