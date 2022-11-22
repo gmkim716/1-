c1039d2f3e5837510a6dfb11efb4596d8287cb94
Jayden, Gyeongmin Kim <starlight3775@gmail.com>
2022-11-22 10:53:36 +0900

    navbar 업데이트

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -1,24 +1,36 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link :to="{ name: 'HomeView' }">Home</router-link> |
-      <router-link :to="{ name: 'LastestMovieView' }">Lastest</router-link> |
-      <router-link :to="{ name: 'PopularMovieView' }">Popular</router-link> |
-      <router-link :to="{ name: 'RecommendView' }">Recommend</router-link> |
-      <router-link :to="{ name: 'UserLikeView' }">User Like</router-link> |
-      <span v-if="!isLogin">
-        <router-link :to="{ name: 'SignupView' }">Signup</router-link> |
-        <router-link :to="{ name: 'LoginView' }">Login</router-link> |
-      </span>
-      <SearchBar/>
-      <span v-if="isLogin">
-        <!-- <router-link :to="{ name: 'ProfileView', params: { pk } }">Profile</router-link> | -->
-        <i @click="profile" class="fa-solid fa-user fa-2x"></i>
-        <!-- <button @click="profile">Profile</button> -->
-        <button @click="logout">Logout</button>
-      </span>
-    </nav>
+  <div id="app" >
     <div class="container">
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+
+      <div class="d-flex justify-content-between collapse navbar-collapse">
+        <div id='navbar-left' class="col col-md-6">
+          <ul class="d-flex justify-content-between navbar-nav">
+            <li class="nav-item nav-link"><router-link :to="{ name: 'HomeView' }">Home</router-link></li>
+            <li class="nav-item nav-link"><router-link :to="{ name: 'LastestMovieView' }">Lastest</router-link></li>
+            <li class="nav-item nav-link"><router-link :to="{ name: 'PopularMovieView' }">Popular</router-link></li>
+            <li class="nav-item nav-link"><router-link :to="{ name: 'RecommendView' }">Recommend</router-link></li>
+            <li class="nav-item nav-link"><router-link :to="{ name: 'UserLikeView' }">User Like</router-link></li>
+          </ul>
+        </div>
+        <div id="navbar-right" class="col col-md-4">
+          <div v-if="!isLogin" >
+            <ul class="d-flex navbar-nav">
+                <SearchBar/>
+              <li class="nav-item nav-link"><router-link :to="{ name: 'LoginView' }">Login</router-link></li>
+              <li class="nav-item nav-link"><router-link :to="{ name: 'SignupView' }">Signup</router-link></li>
+            </ul>
+          </div>
+          <div v-if="isLogin">
+            <!-- <router-link :to="{ name: 'ProfileView', params: { pk } }">Profile</router-link> | -->
+            <i @click="profile" class="fa-solid fa-user fa-2x"></i>
+            <!-- <button @click="profile">Profile</button> -->
+            <button @click="logout">Logout</button>
+          </div>
+        </div>
+      </div>
+    </nav>
       <router-view/>
     </div>
   </div>
